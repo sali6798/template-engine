486976b512a2f9d71f78b3fadf5c50f2362ca890
sali6798 <shaidee.alingcastre@gmail.com>
2020-04-04 23:04:13 -0700

    completed asking user questions about employees

--- app.js
+++ app.js
@@ -4,30 +4,119 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
+
+let employees = [];
+
+function createEmployee() {
+    return [
+        {
+            type: 'input',
+            name: `name`,
+            message: `Name:`,
+        },
+        {
+            type: 'input',
+            name: `id`,
+            message: `id:`,
+        },
+        {
+            type: 'input',
+            name: `email`,
+            message: `Email:`,
+        },
+    ]
+}
+
+function addEngineer() {
+    inquirer.prompt(
+        createEmployee().concat([{
+            type: 'input',
+            name: `github`,
+            message: `Github username:`,
+        }])
+    )
+    .then(({name, id, email, github}) => {
+        const engineer = new Engineer(name, id, email, github);
+        employees.push(engineer);
+    })
+    .then(() => addEmployee())
+}
+
+function addIntern() {
+    inquirer.prompt(
+        createEmployee().concat([{
+            type: 'input',
+            name: `school`,
+            message: `School:`,
+        }])
+    )
+    .then(({name, id, email, school}) => {
+        const intern = new Intern(name, id, email, school);
+        employees.push(intern);
+    })
+    .then(() => addEmployee())
+}
+
+function addEmployee() {
+    inquirer.prompt({
+        type: 'list',
+        name: "employee",
+        message: "Is there any more employees to add?",
+        choices: ['Engineer', 'Intern', 'No one else']
+    }).then(({employee}) => {
+        if (employee === "Engineer") {
+            addEngineer();
+        }
+        else if (employee === "Intern") {
+            addIntern();
+        }
+        else {
+            console.log("Ok, creating summary!");
+            const html = render(employees);
+            fs.writeFileSync(outputPath, html);
+        }
+    })
+}
+
+inquirer.prompt(
+    createEmployee().concat([{
+        type: 'input',
+        name: `officeNumber`,
+        message: `Office Number:`,
+    }])
+)
+.then(({name, id, email, officeNumber}) => {
+    const manager = new Manager(name, id, email, officeNumber);
+    employees.push(manager);
+})
+.then(() => addEmployee())
+
+
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
